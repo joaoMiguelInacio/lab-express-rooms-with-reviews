405d0db1018d41f15fb7c92d8949b13674676f18
Helena Saborit <helena.sb91@gmail.com>
2022-05-21 11:30:50 +0200

    stuck uploading seed

---
+++ rooms-app/seed/seed.js
@@ -0,0 +1,16 @@
+const mongoose = require('../db');
+const Room = require ("../models/Room.model.js");
+const rooms = require ("../data/room-data");
+
+
+const roomsSeed = async () => {
+    try{
+      await Room.create(rooms);
+      console.log(`${rooms.length} rooms created`);
+      await mongoose.connection.close();
+      console.log(`Disconnected from Mongo`);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  roomsSeed();
