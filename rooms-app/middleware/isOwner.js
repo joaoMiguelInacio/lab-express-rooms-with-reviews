dcecb068832a5f31ee05051e452b0329fa4c70ef
Helena Saborit <helena.sb91@gmail.com>
2022-05-21 14:46:58 +0200

    updated

--- rooms-app/middleware/isOwner.js
+++ rooms-app/middleware/isOwner.js
@@ -5,10 +5,10 @@ module.exports = async (req, res, next) => {
     // checks if the user is the owner when trying to access a specific page
     const { id } = req.params;
     const room = await Room.findById(id);
-    const userID = req.session.user._id;
-    const roomOnwerID = room.owner.toString();
-    if (userID == roomOnwerID) {
-        res.render('rooms/rooms-details-owner', room);
-    }
+        const userID = req.session.user._id;
+        const roomOnwerID = room.owner.toString();    
+        if (userID == roomOnwerID) {
+            res.render('rooms/rooms-details-owner', room);
+        }
     next();
   };
