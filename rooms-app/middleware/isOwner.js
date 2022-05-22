152a81caf316e3d400906543c8d310fea8eba097
joaoMiguelInacio <joao.miguel.inacio@gmail.com>
2022-05-22 21:42:51 +0100

    Fixed room details routes and views again

--- rooms-app/middleware/isOwner.js
+++ rooms-app/middleware/isOwner.js
@@ -5,11 +5,19 @@ const Review = require("../models/Review.model.js");
 module.exports = async (req, res, next) => {
     // checks if the user is the owner when trying to access a specific page
     const { id } = req.params;
-    const room = await Room.findById(id).populate("owner reviews");
-        const userID = req.session.user._id;
-        const roomOnwerID = room.owner.toString();    
-        if (userID == roomOnwerID) {
-            res.render('rooms/rooms-details-owner', room);
+    const room = await Room.findById(id)
+        .populate("owner reviews")
+        .populate({
+        path: "reviews",
+        populate:{
+            path: "user"
         }
-    next();
+        });
+    const userID = req.session.user._id;
+    const roomOnwerID = room.owner._id.toString();
+    if (userID == roomOnwerID) {
+        res.render('rooms/rooms-details-owner', room);
+    } else {
+        next();
+    }
   };
