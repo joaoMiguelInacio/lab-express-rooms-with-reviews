3cced607423b999b47c7eb1bf0e5c8b2b0ba67e9
joaoMiguelInacio <joao.miguel.inacio@gmail.com>
2022-05-23 11:36:03 +0100

    review edit and delete done

--- rooms-app/routes/reviews.routes.js
+++ rooms-app/routes/reviews.routes.js
@@ -1,28 +1,49 @@
-/*
+const express = require('express');
+const router = express.Router();
+const Review = require("../models/Review.model.js");
 
-//Create Review
-
-//no need for middleware as only logged in users who are not owners will have access to the view with the review button
-router.get('/:id/reviews-create', async(req, res, next) => {
-  const {id} = req.params;
-  const room = await Room.findById(id);
-  res.render('room/reviews-create', room);
-});
-
-router.post('/:id/create-review-url', async (req, res, next) => {
-  try{
-      const { content } = req.body;
-      const { id } = req.params;
-      const newReview = await Review.create ({
-         content: content,
-         movie: id
-      });
-      const newReviewId = newReview._id;
-      await Movie.findByIdAndUpdate(id, { $addToSet: { reviews: newReviewId } });
-      res.redirect(`/movie/${id}/details-url`); 
-  } catch (error) {
-      next (error);
+router.get('/reviews-list', async (req, res, next) => {
+  try {
+    const reviews = await Review.find().populate('user');
+    res.render('reviews/reviews-list', {reviews});
+  } catch (err) {
+    next(err);
   }
 });
 
-*/
+router.get('/:id/reviews-edit', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const review = await Review.findById (id);
+    res.render ('reviews/reviews-edit', review);
+  } catch(error){
+    next(error);
+  }
+});
+
+//missing middleware so that only owner can edit own review
+//redirect to room details page and not to rooms-list or reviews-list
+router.post('/:id/reviews-edit', async (req, res, next) => {
+  try {
+		const { id } = req.params;
+		const { comment } = req.body;
+		await Review.findByIdAndUpdate(id, { comment }, { new: true });
+		res.redirect("/rooms/rooms-list");
+	} catch(error){
+		next(error);
+	}
+});
+
+//missing middleware so that only owner can delete own review
+//redirect to room details page and not to rooms-list or reviews-list
+router.post('/:id/reviews-delete', async (req, res, next) => {
+  try {
+		const { id } = req.params;
+		await Review.findByIdAndDelete(id);
+    res.redirect("/rooms/rooms-list");
+	} catch (error) {
+		next(error);
+	}
+});
+
+module.exports = router;
